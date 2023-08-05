35ee1a19bfa4f97c648e9efc83c23d91c33c5b50
FullBread <FullRe@yandex.ru>
2023-08-05 03:42:58 +0300

    fix 6

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,18 +1,18 @@
 package hexlet.code.schemas;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema {
-    protected List<Predicate<Object>> validations = new LinkedList<>();
+    protected List<Predicate<Object>> validations = new ArrayList<>();
     protected boolean isRequired = false;
     public final boolean isValid(Object value) {
         if (value == null) {
             return !isRequired;
         }
         for (Predicate<Object> validation: validations) {
-            if (!(validation.test(value))) {
+            if (!validation.test(value)) {
                 return false;
             }
         }
